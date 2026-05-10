7430a49ae30cab94717f09be8795c2d296a3c8ad
Zohaib479 <don462671@gmail.com>
2026-05-10 15:00:18 +0500

    for deployment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     >
       <div className="flex items-center gap-2">
         <div className="w-10 h-10 bg-cyber-cyan rounded-lg flex items-center justify-center animate-glow-pulse">
-          <span className="text-black font-bold text-xl">T</span>
+          
         </div>
         <span className="text-2xl font-black tracking-tighter text-white">TRY<span className="text-cyber-cyan">ONIX</span></span>
       </div>
